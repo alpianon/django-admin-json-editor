123ae963ae46427e9c90823b0dc4ffbdfe663d1d
Alberto Pianon <alberto@pianon.eu>
2018-12-05 16:56:34 +0100

    reverted to commit #3d300df

--- django_admin_json_editor/static/django_admin_json_editor/editor.js
+++ django_admin_json_editor/static/django_admin_json_editor/editor.js
@@ -1,10 +1,11 @@
-var editor = {};
+var editor = {}
+var $ = django.jQuery
 var enable_json_editor = function(element){
-  var id = element.id;
-  if (id.indexOf('__prefix__') === -1) { // instantiate editor only if it is not an inline template row
-    var textarea_id = "id_"+element.id.replace('_editor', '');
-    var data = JSON.parse(element.getAttribute('data-data'));
-    var options = JSON.parse(element.getAttribute('data-options'));
+  var id = element.id
+  if (id.indexOf('__prefix__') === -1) { // activate editor only if it is not an inline template row
+    var textarea_id = "id_"+element.id.replace('_editor', '')
+    var data = JSON.parse(element.getAttribute('data-data'))
+    var options = JSON.parse(element.getAttribute('data-options'))
     editor[id] = new JSONEditor(element, options);
     JSONEditor.plugins.sceditor.emoticonsEnabled = element.getAttribute("data-sceditor");
     editor[id].on('change', function () {
@@ -15,13 +16,14 @@ var enable_json_editor = function(element){
         else {
             var json = editor[id].getValue();
             $('#'+textarea_id)[0].value = JSON.stringify(json);
-        };
+        }
     });
     if (data != null) {
       editor[id].setValue(data);
-    };
-  };
-};
+    }
+    
+  }
+}
 
 $(document).ready(function() {
   $(document).find("[id$=_editor]").each(function(){
@@ -33,4 +35,5 @@ $(document).on('formset:added', function(event, $row, formsetName) {
   $row.find("[id$=_editor]").each(function(){
     enable_json_editor(this);      
   });  
+    
 });
--- django_admin_json_editor/static/django_admin_json_editor/jquery.init.js
+++
@@ -1,3 +0,0 @@
-var jQuery = django.jQuery;
-var $ = django.jQuery;
-
